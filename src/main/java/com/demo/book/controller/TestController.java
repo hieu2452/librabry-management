567194f9f7c02f5917e31a9549ee3b543daa2d08
HIEU <hieu245202@gmail.com>
2024-03-18 00:36:11 -0100

    kafka config

--- src/main/java/com/demo/book/controller/TestController.java
+++ src/main/java/com/demo/book/controller/TestController.java
@@ -2,6 +2,7 @@ package com.demo.book.controller;
 
 import com.demo.book.config.MessageProducer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,11 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
     @Autowired
-    private MessageProducer messageProducer;
+    private KafkaTemplate<String,String> kafkaTemplate;
 
     @GetMapping("send")
     public String sendMessage(@RequestParam(defaultValue = "") String message) {
-        messageProducer.sendMessage("test-consumer-group", message);
+        kafkaTemplate.send("test-consumer-group-0", message);
 
         return message;
     }
