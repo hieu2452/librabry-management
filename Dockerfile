fe8ee5ca263891756522655d7b0f3ca4a02e40c1
hieu2452 <hieu245202@gmail.com>
2024-03-20 16:39:03 +0700

    fixed test case

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM eclipse-temurin:17-jdk-alpine
+FROM eclipse-temurin:11-jdk-alpine
 
 VOLUME /tmp
 
-COPY target/server-0.0.1-SNAPSHOT.jar /app.jar
+COPY target/*.jar /app.jar
 
 EXPOSE 8080
 
--- src/main/java/com/demo/book/config/KafkaProducerConfig.java
+++ src/main/java/com/demo/book/config/KafkaProducerConfig.java
@@ -1,46 +1,46 @@
-package com.demo.book.config;
-
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.*;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@EnableKafka
-@Configuration
-public class KafkaProducerConfig {
-    @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.30.214.45:9093");
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.LINGER_MS_CONFIG, 2000);
-        configProps.put(ProducerConfig.ACKS_CONFIG,"all");
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
-    @Bean
-    public KafkaAdmin admin() {
-        return new KafkaAdmin(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "172.30.214.45:9092,172.30.214.45:9093,172.30.214.45:9094"));
-    }
-
-    @Bean
-    public NewTopic adviceTopic() {
-        return TopicBuilder.name("test-notification")
-                .partitions(3)
-                .replicas(3)
-                .build();
-    }
-}
+//package com.demo.book.config;
+//
+//import org.apache.kafka.clients.admin.AdminClientConfig;
+//import org.apache.kafka.clients.admin.NewTopic;
+//import org.apache.kafka.clients.producer.ProducerConfig;
+//import org.apache.kafka.common.serialization.StringSerializer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.annotation.EnableKafka;
+//import org.springframework.kafka.config.TopicBuilder;
+//import org.springframework.kafka.core.*;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@EnableKafka
+//@Configuration
+//public class KafkaProducerConfig {
+//    @Bean
+//    public ProducerFactory<String, String> producerFactory() {
+//        Map<String, Object> configProps = new HashMap<>();
+//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.30.214.45:9093");
+//        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        configProps.put(ProducerConfig.LINGER_MS_CONFIG, 2000);
+//        configProps.put(ProducerConfig.ACKS_CONFIG,"all");
+//        return new DefaultKafkaProducerFactory<>(configProps);
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String, String> kafkaTemplate() {
+//        return new KafkaTemplate<>(producerFactory());
+//    }
+//    @Bean
+//    public KafkaAdmin admin() {
+//        return new KafkaAdmin(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "172.30.214.45:9092,172.30.214.45:9093,172.30.214.45:9094"));
+//    }
+//
+//    @Bean
+//    public NewTopic adviceTopic() {
+//        return TopicBuilder.name("test-notification")
+//                .partitions(3)
+//                .replicas(3)
+//                .build();
+//    }
+//}
